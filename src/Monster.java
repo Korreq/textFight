932a7dab2c7f89628bbceb04f056ca9a05d48dba
RuvikRuben <falconkrwisty@gmail.com>
2020-11-05 16:48:18 +0100

    Potwory 2

--- src/Monster.java
+++ src/Monster.java
@@ -1,79 +1,231 @@
 public interface Monster {
-    void Hp();
-    void ATK();
-    void Def();
-    void Mdef();
+    int Hp();
+    int ATK();
+    int Def();
+    int Mdef();
 }
-
 
 class L1 implements Monster
 {
 
-    @Override
-    public void Hp() {
-
+    public int Hp() {
+        return 20;
     }
 
-    @Override
-    public void ATK() {
-
+    public int ATK() {
+        return 31;
     }
 
-    @Override
-    public void Def() {
-
+    public int Def() {
+        return 0;
     }
 
-    @Override
-    public void Mdef() {
-
+    public int Mdef() {
+        return 0;
     }
 }
 
 class S1 implements Monster
 {
 
-    @Override
-    public void Hp() {
-
+    public int Hp() {
+        return 25;
     }
 
-    @Override
-    public void ATK() {
-
+    public int ATK() {
+        return 21;
     }
 
-    @Override
-    public void Def() {
-
+    public int Def() {
+        return 0;
     }
 
-    @Override
-    public void Mdef() {
-
+    public int Mdef() {
+        return 0;
     }
 }
 
 class M1 implements Monster
 {
 
-    @Override
-    public void Hp() {
 
+    public int Hp() {
+        return 20;
     }
 
-    @Override
-    public void ATK() {
-
+    public int ATK() {
+        return 21;
     }
 
-    @Override
-    public void Def() {
-
+    public int Def() {
+        return 0;
     }
 
-    @Override
-    public void Mdef() {
+    public int Mdef() {
+        return 0;
+    }
+}
 
+class L2 implements Monster
+{
+
+    public int Hp() {
+        return 30;
+    }
+
+    public int ATK() {
+        return 37;
+    }
+
+    public int Def() {
+        return 0;
+    }
+
+    public int Mdef() {
+        return 0;
+    }
+}
+
+class S2 implements Monster
+{
+
+    public int Hp() {
+        return 40;
+    }
+
+    public int ATK() {
+        return 27;
+    }
+
+    public int Def() {
+        return 0;
+    }
+
+    public int Mdef() {
+        return 0;
+    }
+}
+
+class M2 implements Monster
+{
+
+
+    public int Hp() {
+        return 35;
+    }
+
+    public int ATK() {
+        return 27;
+    }
+
+    public int Def() {
+        return 0;
+    }
+
+    public int Mdef() {
+        return 0;
+    }
+}
+
+class L3 implements Monster
+{
+
+    public int Hp() {
+        return 40;
+    }
+
+    public int ATK() {
+        return 43;
+    }
+
+    public int Def() {
+        return 0;
+    }
+
+    public int Mdef() {
+        return 0;
+    }
+}
+
+class S3 implements Monster
+{
+
+    public int Hp() {
+        return 55;
+    }
+
+    public int ATK() {
+        return 34;
+    }
+
+    public int Def() {
+        return 0;
+    }
+
+    public int Mdef() {
+        return 0;
+    }
+}
+
+class M3 implements Monster
+{
+
+
+    public int Hp() {
+        return 50;
+    }
+
+    public int ATK() {
+        return 34;
+    }
+
+    public int Def() {
+        return 0;
+    }
+
+    public int Mdef() {
+        return 0;
+    }
+}
+
+class B1 implements Monster
+{
+
+
+    public int Hp() {
+        return 150;
+    }
+
+    public int ATK() {
+        return 31;
+    }
+
+    public int Def() {
+        return 5;
+    }
+
+    public int Mdef() {
+        return 5;
+    }
+}
+
+class B2 implements Monster
+{
+
+
+    public int Hp() {
+        return 250;
+    }
+
+    public int ATK() {
+        return 41;
+    }
+
+    public int Def() {
+        return 10;
+    }
+
+    public int Mdef() {
+        return 10;
     }
 }
