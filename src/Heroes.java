66b4960022494bb9a9483d93c93c10308256f53f
Korreq <55250560+Korreq@users.noreply.github.com>
2020-11-05 16:40:46 +0100

    Bog

--- src/Heroes.java
+++ src/Heroes.java
@@ -1,20 +1,100 @@
-import java.util.Random;
-
 public interface Heroes {
 
-    void hp();
-    void dex();
-    void armor();
-
+    int hp();
+    int dex();
+    int armor();
+    boolean mage();
 
 }
 
 class Warrior implements Heroes{
 
-    public void hp(){};
-    public void dex(){};
-    public void armor(){};
+    public int hp(){
+        return 30;
+    }
+    public int dex(){
+        return 0;
+    }
+    public int armor(){
+        return 15;
+    }
+    public boolean mage(){
+        return false;
+    }
 
 
+}
+
+class Rouge implements Heroes{
+
+    public int hp(){
+        return 0;
+    }
+    public int dex(){
+        return 10;
+    }
+    public int armor(){
+        
+    }
+    public boolean mage(){
+        return false;
+    }
+
+
+}
+
+class Bard implements Heroes{
+
+    public int hp(){
+        return 0;
+    }
+    public int dex(){}
+    public int armor(){}
+    public boolean mage(){
+        return false;
+    }
+
+}
+
+class Paladyn implements Heroes{
+
+    public int hp(){
+        return 25;
+    }
+    public int dex(){
+        return 0;
+    }
+    public int armor(){
+        return 10;
+    }
+    public boolean mage(){
+        return true;
+    }
+
+}
+
+class Mage implements Heroes{
+
+    public int hp(){
+        return 0;
+    }
+    public int dex(){}
+    public int armor(){}
+    public boolean mage(){
+        return true;
+    };
+
+}
+
+class Cleric implements Heroes{
+
+    public int hp(){
+        return 0;
+    }
+    public int dex(){}
+    public int armor(){}
+    public boolean mage(){
+        return true;
+    };
 
 }
