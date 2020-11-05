b713619c6d6cce3f737e1077d44dd2ae78eed225
Korreq <55250560+Korreq@users.noreply.github.com>
2020-11-05 16:23:35 +0100

    BOHATEROWIE

---
+++ src/Heroes.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public interface Heroes {
+
+    int hp;
+    int dex;
+    int armor;
+
+
+}
+
+class Warrior implements Heroes{
+
+
+    hp = 20;
+    dex = 0;
+    armor = 10;
+
+
+}
