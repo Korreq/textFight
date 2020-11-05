36fd56e0235c4649ae3c78b65ca953fd2a3b0a24
Korreq <55250560+Korreq@users.noreply.github.com>
2020-11-05 17:01:40 +0100

    BIg G

---
+++ src/Runy/Runy.java
@@ -0,0 +1,19 @@
+package Runy;
+
+public interface Runy {
+
+    void fire();
+    void water();
+    void wind();
+    void earth();
+    void thunder();
+    void light();
+    void blood();
+    void steel();
+    void flint();
+    void wolf();
+
+
+
+
+}
