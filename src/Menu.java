dd5ae6693e3540bfac9271adf8fdb95d11cb7fa3
Korreq <55250560+Korreq@users.noreply.github.com>
2020-11-10 16:34:04 +0100

    Selling Dokney Version

--- src/Menu.java
+++ src/Menu.java
@@ -21,6 +21,7 @@ public class Menu {
             System.out.println("1 : Nowa gra");
             System.out.println("2 : Credits");
             System.out.println("3 : Wyjdz");
+            System.out.println("\nv. 0.01(Selling Donkey)");
             int wybór = (scanner.nextInt());
             switch (wybór) {
                 case (1) -> {
