33f0e74fa9db215225a6f5e3fa0512a8b4dc9edf
Korreq <55250560+Korreq@users.noreply.github.com>
2020-11-11 14:57:19 +0100

    fix

--- src/Bohaterowie/TablicaBohaterow.java
+++ src/Bohaterowie/TablicaBohaterow.java
@@ -52,52 +52,7 @@ public class TablicaBohaterow {
         private Bohaterowie w35 = new Bohaterowie("Ezekiel","Kapłan","light",30,27,-1,6,10,true,"W");
         private Bohaterowie w36 = new Bohaterowie("w36","Kapłan","blood",30,26,-2,6,20,true,"W");
 
-        private Bohaterowie w1 = new Bohaterowie("Albert","Wojownik","steel",53,18,7,16,false,"A");
-        private Bohaterowie w2 = new Bohaterowie("Zabiast","Wojownik","flint",44,18,8,16,false,"Z");
-        private Bohaterowie w3 = new Bohaterowie("w3","Wojownik","wolf",44,18,6,16,false,"W");
-        private Bohaterowie w4 = new Bohaterowie("w4","Wojownik","steel",53,20,4,16,false,"W");
-        private Bohaterowie w5 = new Bohaterowie("w5","Wojownik","flint",44,16,8,16,false,"W");
-        private Bohaterowie w6 = new Bohaterowie("w6","Wojownik","steel",53,21,5,16,false,"W");
-        private Bohaterowie w7 = new Bohaterowie("Bobert","Wojownik","wolf",44,18,4,16,false,"W");
-
-
-        private Bohaterowie w8 = new Bohaterowie("Robert","Paladyn","earth",40,16,7,10,true,"R");
-        private Bohaterowie w9 = new Bohaterowie("Kresul","Paladyn","steel",48,16,8,10,true,"K");
-        private Bohaterowie w10 = new Bohaterowie("w10","Paladyn","water",40,16,6,10,true,"W");
-        private Bohaterowie w11 = new Bohaterowie("w11","Paladyn","steel",48,18,4,10,true,"W");
-        private Bohaterowie w12 = new Bohaterowie("w12","Paladyn","fire",40,14,8,10,true,"W");
-        private Bohaterowie w13 = new Bohaterowie("w13","Paladyn","steel",48,19,5,10,true,"W");
-        private Bohaterowie w14 = new Bohaterowie("w14","Paladyn","wind",40,16,4,10,true,"W");
-
-
-        private Bohaterowie w15 = new Bohaterowie("Robert","Łotrzyk","steel",43,26,5,12,false,"R");
-        private Bohaterowie w16 = new Bohaterowie("Twardo","Łotrzyk","flint",35,25,3,12,false,"T");
-        private Bohaterowie w17 = new Bohaterowie("w17","Łotrzyk","wolf",35,27,4,12,false,"W");
-        private Bohaterowie w18 = new Bohaterowie("w18","Łotrzyk","steel",43,27,2,12,false,"W");
-        private Bohaterowie w19 = new Bohaterowie("w19","Łotrzyk","flint",35,28,2,12,false,"W");
-        private Bohaterowie w20 = new Bohaterowie("w20","Łotrzyk","steel",43,25,0,12,false,"W");
-
-
-        private Bohaterowie w21 = new Bohaterowie("Jan","Bard","wolf",30,28,5,8,false,"J");
-        private Bohaterowie w22 = new Bohaterowie("Włodziu","Bard","flint",30,29,8,8,false,"W");
-        private Bohaterowie w23 = new Bohaterowie("w23","Bard","steel",36,30,2,8,false,"W");
-        private Bohaterowie w24 = new Bohaterowie("w24","Bard","wolf",30,27,0,8,false,"W");
-
-
-        private Bohaterowie w25 = new Bohaterowie("Ezekiel","Mag","fire",29,23,2,6,true,"E");
-        private Bohaterowie w26 = new Bohaterowie("Algird","Mag","water",29,22,1,6,true,"A");
-        private Bohaterowie w27 = new Bohaterowie("w27","Mag","wind",29,23,2,6,true,"W");
-        private Bohaterowie w28 = new Bohaterowie("w28","Mag","earth",29,22,1,6,true,"W");
-        private Bohaterowie w29 = new Bohaterowie("w29","Mag","fire",29,25,-1,6,true,"W");
-        private Bohaterowie w30 = new Bohaterowie("w30","Mag","water",29,24,-2,6,true,"W");
-        private Bohaterowie w31 = new Bohaterowie("w31","Mag","wind",29,25,-1,6,true,"W");
-        private Bohaterowie w32 = new Bohaterowie("w32","Mag","earth",29,24,-2,6,true,"W");
-
-
-        private Bohaterowie w33 = new Bohaterowie("Darek","Kapłan","light",30,25,2,6,true,"D");
-        private Bohaterowie w34 = new Bohaterowie("Darionha","Kapłan","blood",30,24,1,6,true,"D");
-        private Bohaterowie w35 = new Bohaterowie("Ezekiel","Kapłan","light",30,27,-1,6,true,"W");
-        private Bohaterowie w36 = new Bohaterowie("w36","Kapłan","blood",30,26,-2,6,true,"W");
+        
 
 
         private Bohaterowie[] boh = {w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15,w16,w17,w18,w19,w20,w21,w22,w23,w24,w25,w26,w27,w28,w29,w30,w31,w32,w33,w34,w35,w36};
