0b610591ea16185e7e33e2a96ad2ac9612eaaa35
RuvikRuben <falconkrwisty@gmail.com>
2020-11-16 17:37:11 +0100

    ini

--- src/Sortowanie.java
+++ src/Sortowanie.java
@@ -7,6 +7,6 @@ public class Sortowanie implements Comparator<Bohaterowie>
     public int compare(Bohaterowie p1, Bohaterowie p2)
     {
         if(p2 == null) return -1;
-        return Integer.compare(p1.getDex(), p2.getDex());
+        return Integer.compare(p2.getDex(), p1.getDex());
     }
 }
