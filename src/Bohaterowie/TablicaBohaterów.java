aec6972f2caf0bdda5fa21543b68594bb3482f51
Korreq <55250560+Korreq@users.noreply.github.com>
2020-11-16 14:14:18 +0100

    Update TablicaBohaterów.java

--- src/Bohaterowie/TablicaBohaterów.java
+++ src/Bohaterowie/TablicaBohaterów.java
@@ -2,52 +2,52 @@ package Bohaterowie;
 
 public class TablicaBohaterów {
 
-        public Bohaterowie w1 = new Bohaterowie("Albert","Wojownik","steel",53,18,7,16,false,"A","");
-        public Bohaterowie w2 = new Bohaterowie("Zabiast","Wojownik","flint",44,18,8,16,false,"Z","");
-        public Bohaterowie w3 = new Bohaterowie("Alfred","Wojownik","wolf",44,18,6,16,false,"W","");
-        public Bohaterowie w4 = new Bohaterowie("w4","Wojownik","steel",53,20,4,16,false,"W","");
-        public Bohaterowie w5 = new Bohaterowie("Dred","Wojownik","flint",44,16,8,16,false,"W","");
-        public Bohaterowie w6 = new Bohaterowie("w6","Wojownik","steel",53,21,5,16,false,"W","");
-        public Bohaterowie w7 = new Bohaterowie("Bobert","Wojownik","wolf",44,18,4,16,false,"B","");
+        public Bohaterowie w1 = new Bohaterowie("Albert","Wojownik","steel",53,5,7,16,false,"A","");
+        public Bohaterowie w2 = new Bohaterowie("Zabiast","Wojownik","flint",44,5,8,16,false,"Z","");
+        public Bohaterowie w3 = new Bohaterowie("Alfred","Wojownik","wolf",44,5,6,16,false,"A","");
+        public Bohaterowie w4 = new Bohaterowie("w4","Wojownik","steel",53,5,4,16,false,"W","");
+        public Bohaterowie w5 = new Bohaterowie("Dred","Wojownik","flint",44,5,8,16,false,"D","");
+        public Bohaterowie w6 = new Bohaterowie("w6","Wojownik","steel",53,5,5,16,false,"W","");
+        public Bohaterowie w7 = new Bohaterowie("Bobert","Wojownik","wolf",44,5,4,16,false,"B","");
 
 
-        public Bohaterowie w8 = new Bohaterowie("Robert","Paladyn","earth",40,16,7,10,true,"R","");
-        public Bohaterowie w9 = new Bohaterowie("Kresul","Paladyn","steel",48,16,8,10,true,"K","");
-        public Bohaterowie w10 = new Bohaterowie("Vintael","Paladyn","water",40,16,6,10,true,"W","");
-        public Bohaterowie w11 = new Bohaterowie("w11","Paladyn","steel",48,18,4,10,true,"W","");
-        public Bohaterowie w12 = new Bohaterowie("w12","Paladyn","fire",40,14,8,10,true,"W","");
-        public Bohaterowie w13 = new Bohaterowie("w13","Paladyn","steel",48,19,5,10,true,"W","");
-        public Bohaterowie w14 = new Bohaterowie("w14","Paladyn","wind",40,16,4,10,true,"W","");
+        public Bohaterowie w8 = new Bohaterowie("Robert","Paladyn","earth",40,0,7,10,true,"R","");
+        public Bohaterowie w9 = new Bohaterowie("Kresul","Paladyn","steel",48,0,8,10,true,"K","");
+        public Bohaterowie w10 = new Bohaterowie("Vintael","Paladyn","water",40,0,6,10,true,"V","");
+        public Bohaterowie w11 = new Bohaterowie("w11","Paladyn","steel",48,0,4,10,true,"W","");
+        public Bohaterowie w12 = new Bohaterowie("w12","Paladyn","fire",40,0,8,10,true,"W","");
+        public Bohaterowie w13 = new Bohaterowie("w13","Paladyn","steel",48,0,5,10,true,"W","");
+        public Bohaterowie w14 = new Bohaterowie("w14","Paladyn","wind",40,0,4,10,true,"W","");
 
 
-        public Bohaterowie w15 = new Bohaterowie("Jacob","Łotrzyk","steel",43,26,5,12,false,"R","");
-        public Bohaterowie w16 = new Bohaterowie("Twardo","Łotrzyk","flint",35,25,3,12,false,"T","");
-        public Bohaterowie w17 = new Bohaterowie("","Łotrzyk","wolf",35,27,4,12,false,"W","");
-        public Bohaterowie w18 = new Bohaterowie("w18","Łotrzyk","steel",43,27,2,12,false,"W","");
-        public Bohaterowie w19 = new Bohaterowie("w19","Łotrzyk","flint",35,28,2,12,false,"W","");
-        public Bohaterowie w20 = new Bohaterowie("Goldlion","Łotrzyk","steel",43,25,0,12,false,"W","");
+        public Bohaterowie w15 = new Bohaterowie("Jacob","Łotrzyk","steel",43,20,5,12,false,"J","");
+        public Bohaterowie w16 = new Bohaterowie("Twardo","Łotrzyk","flint",35,20,3,12,false,"T","");
+        public Bohaterowie w17 = new Bohaterowie("","Łotrzyk","wolf",35,20,4,12,false,"?","");
+        public Bohaterowie w18 = new Bohaterowie("w18","Łotrzyk","steel",43,20,2,12,false,"W","");
+        public Bohaterowie w19 = new Bohaterowie("w19","Łotrzyk","flint",35,20,2,12,false,"W","");
+        public Bohaterowie w20 = new Bohaterowie("Goldlion","Łotrzyk","steel",43,20,0,12,false,"G","");
 
 
-        public Bohaterowie w21 = new Bohaterowie("Hiacynt","Bard","wolf",30,28,5,8,false,"J","");
-        public Bohaterowie w22 = new Bohaterowie("Włodziu","Bard","flint",30,29,2,8,false,"W","");
-        public Bohaterowie w23 = new Bohaterowie("Ur","Bard","steel",36,30,2,8,false,"W","");
-        public Bohaterowie w24 = new Bohaterowie("Benek","Bard","wolf",30,27,0,8,false,"W","");
+        public Bohaterowie w21 = new Bohaterowie("Hiacynt","Bard","wolf",30,15,5,8,false,"H","");
+        public Bohaterowie w22 = new Bohaterowie("Rass","Bard","flint",30,15,2,8,false,"R","");
+        public Bohaterowie w23 = new Bohaterowie("Ur","Bard","steel",36,15,2,8,false,"U","");
+        public Bohaterowie w24 = new Bohaterowie("Benek","Bard","wolf",30,15,0,8,false,"B","");
 
 
-        public Bohaterowie w25 = new Bohaterowie("Ezekiel","Mag","fire",29,23,2,6,true,"E","");
-        public Bohaterowie w26 = new Bohaterowie("Algird","Mag","water",29,22,1,6,true,"A","");
-        public Bohaterowie w27 = new Bohaterowie("w27","Mag","wind",29,23,2,6,true,"W","");
-        public Bohaterowie w28 = new Bohaterowie("w28","Mag","earth",29,22,1,6,true,"W","");
-        public Bohaterowie w29 = new Bohaterowie("w29","Mag","fire",29,25,-1,6,true,"W","");
-        public Bohaterowie w30 = new Bohaterowie("w30","Mag","water",29,24,-2,6,true,"W","");
-        public Bohaterowie w31 = new Bohaterowie("w31","Mag","wind",29,25,-1,6,true,"W","");
-        public Bohaterowie w32 = new Bohaterowie("w32","Mag","earth",29,24,-2,6,true,"W","");
+        public Bohaterowie w25 = new Bohaterowie("Ezekiel","Mag","fire",29,10,0,6,true,"E","");
+        public Bohaterowie w26 = new Bohaterowie("Algird","Mag","water",29,10,0,6,true,"A","");
+        public Bohaterowie w27 = new Bohaterowie("w27","Mag","wind",29,10,0,6,true,"W","");
+        public Bohaterowie w28 = new Bohaterowie("w28","Mag","earth",29,10,0,6,true,"W","");
+        public Bohaterowie w29 = new Bohaterowie("w29","Mag","fire",29,10,0,6,true,"W","");
+        public Bohaterowie w30 = new Bohaterowie("w30","Mag","water",29,10,0,6,true,"W","");
+        public Bohaterowie w31 = new Bohaterowie("w31","Mag","wind",29,10,0,6,true,"W","");
+        public Bohaterowie w32 = new Bohaterowie("w32","Mag","earth",29,10,0,6,true,"W","");
 
 
-        public Bohaterowie w33 = new Bohaterowie("Darek","Kapłan","light",30,25,2,6,true,"D","");
-        public Bohaterowie w34 = new Bohaterowie("Darionha","Kapłan","blood",30,24,1,6,true,"D","");
-        public Bohaterowie w35 = new Bohaterowie("Ezekiel","Kapłan","light",30,27,-1,6,true,"W","");
-        public Bohaterowie w36 = new Bohaterowie("N'fz","Kapłan","blood",30,26,-2,6,true,"W","");
+        public Bohaterowie w33 = new Bohaterowie("N'fz","Kapłan","light",30,10,0,6,true,"N","");
+        public Bohaterowie w34 = new Bohaterowie("Darionha","Kapłan","blood",30,10,0,6,true,"D","");
+        public Bohaterowie w35 = new Bohaterowie("Ezekiel","Kapłan","light",30,10,0,6,true,"E","");
+        public Bohaterowie w36 = new Bohaterowie("Darek","Kapłan","blood",30,10,0,6,true,"D","");
 
         public Bohaterowie[] boh = {w1,w2,w3,w4,w5,w6,w7,w8,w9,w10,w11,w12,w13,w14,w15,w16,w17,w18,w19,w20,w21,w22,w23,w24,w25,w26,w27,w28,w29,w30,w31,w32,w33,w34,w35,w36};
 
