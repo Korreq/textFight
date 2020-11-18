cffe2845d74e9eaca2626626a8b8d4ca12bcf65a
Prezes <PrezesGithub@gmail.com>
2020-11-18 15:00:27 +0100

    MenuMagia

--- src/Menu.java
+++ src/Menu.java
@@ -62,7 +62,6 @@ public class Menu
                             System.out.println("║   ║ - - - - - - - - - - - - - - - - -  ║");
                             System.out.println("║ 1 ║ Podstawy gry                       ║");
                             System.out.println("║ 2 ║ Magia (runy)                       ║");
-                            System.out.println("║ 3 ║ Klasy                              ║");
                             System.out.println("╚═══╩════════════════════════════════════╝");
                             int choice2;
                             choice2 = scanner.nextInt();
@@ -84,11 +83,103 @@ public class Menu
                                     System.out.println("Walka dobiega końca gdy wszyscy twoi lub wrodzy bohaterowie zostaną zabici.");
                                 }
                                 case (2) -> {
-                                    
-                                }
-                                case (3) -> {
+                                        System.out.println("╔═══╦════════════════════════════════════╗");
+                                        System.out.println("║   ║          Wybierz runę              ║");
+                                        System.out.println("║   ║ - - - - - - - - - - - - - - - - -  ║");
+                                        System.out.println("║ 1 ║ Runa ognia                         ║");
+                                        System.out.println("║ 2 ║ Runa wody                          ║");
+                                        System.out.println("║ 3 ║ Runa wiatru                        ║");
+                                        System.out.println("║ 4 ║ Runa ziemi                         ║");
+                                        System.out.println("║ 5 ║ Runa światła                       ║");
+                                        System.out.println("║ 6 ║ Runa krwi                          ║");
+                                        System.out.println("║ 7 ║ Runa błyskawic                     ║");
+                                        System.out.println("╚═══╩════════════════════════════════════╝");
+                                    int choice3;
+                                    choice3 = scanner.nextInt();
+                                    switch (choice3)
+                                    {
+                                        case (1) -> {
+                                            System.out.println("╔═══════════════════════════════════════════════════════════════════════════════════════════════╗");
+                                            System.out.println("║                                          Magia Ognia                                          ║");
+                                            System.out.println("╠═══════════════════════════════════════════════════════════════════════════════════════════════╣");
+                                            System.out.println("║ Obszarowe obrażenia.                                                                          ║");
+                                            System.out.println("║ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ║");
+                                            System.out.println("║ 1. Ognista włócznia - średnie obrażenia zadane dla danej kolumny przeciwników.                ║");
+                                            System.out.println("║ 2. Ściana ognia - średnie obrażenia dla danego rzędu przeciwników.                            ║");
+                                            System.out.println("║ 3. Deszcz ognia - duże obrażenia  zadane wszystkim przeciwnikom na polu bitwy.                ║");
+                                            System.out.println("╚═══════════════════════════════════════════════════════════════════════════════════════════════╝");
+                                        }
+                                        case (2) -> {
+                                            System.out.println("╔═══════════════════════════════════════════════════════════════════════════════════════════════╗");
+                                            System.out.println("║                                          Magia Wody                                           ║");
+                                            System.out.println("╠═══════════════════════════════════════════════════════════════════════════════════════════════╣");
+                                            System.out.println("║ Odnawianie sił życiowych i zaklęcia defensywne.                                               ║");
+                                            System.out.println("║ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ║");
+                                            System.out.println("║ 1. Kropla życia - jeden sojusznik odzyskuje część życia.                                      ║");
+                                            System.out.println("║ 2. Mgła - jeden sojusznik zostaje osłoniony mgłą, przez co unika trafień w najbliższej turze. ║");
+                                            System.out.println("║ 3. Fala życia  - wszyscy sojusznicy odzyskują pełne życie.                                    ║");
+                                            System.out.println("╚═══════════════════════════════════════════════════════════════════════════════════════════════╝");
+                                        }
+                                        case (3) -> {
+                                            System.out.println("╔═══════════════════════════════════════════════════════════════════════════════════════════════╗");
+                                            System.out.println("║                                         Magia Wiatru                                          ║");
+                                            System.out.println("╠═══════════════════════════════════════════════════════════════════════════════════════════════╣");
+                                            System.out.println("║ Zaklęcia hybrydowe.                                                                           ║");
+                                            System.out.println("║ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ║");
+                                            System.out.println("║ 1. Świst wiatru - jeden przeciwnik otrzymuje średnią ilość obrażeń.                           ║");
+                                            System.out.println("║ 2. Ciepła bryza - jeden sojusznik odzyskuje część życia.                                      ║");
+                                            System.out.println("║ 3. Trąba powietrzna - wszyscy wrogowie otrzymują duże obrażenia.                              ║");
+                                            System.out.println("╚═══════════════════════════════════════════════════════════════════════════════════════════════╝");
+                                        }
+                                        case (4) -> {
+                                            System.out.println("╔═══════════════════════════════════════════════════════════════════════════════════════════════╗");
+                                            System.out.println("║                                          Magia Ziemi                                          ║);
+                                            System.out.println("╠═══════════════════════════════════════════════════════════════════════════════════════════════╣");
+                                            System.out.println("║ Zaklęcia defensywne.                                                                          ║");
+                                            System.out.println("║ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ║");
+                                            System.out.println("║ 1. Kokon - wskazany sojusz otrzymuje połowę mniejsze obrażenia w następnej turze.             ║");
+                                            System.out.println("║ 2. Pnącza - wskazany wróg zostaje oplątany na jedną turę i nie może wykonać żadnej akcji.     ║");
+                                            System.out.println("║ 3.  Całun flory - cała twoja drużyna otrzymuje połowę mniejsze obrażenia.                     ║");
+                                            System.out.println("╚═══════════════════════════════════════════════════════════════════════════════════════════════╝");
+                                        }
+                                        case (5) -> {
+                                            System.out.println("╔════════════════════════════════════════════════════════════════════════════════════════════════╗");
+                                            System.out.println("║                                         Magia Światła                                          ║");
+                                            System.out.println("╠════════════════════════════════════════════════════════════════════════════════════════════════╣");
+                                            System.out.println("║ Zaklęcia kontrolne.                                                                            ║");
+                                            System.out.println("║ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  ║");
+                                            System.out.println("║ 1. Boska pomoc - ulecz wskazanego sojusznika.                                                  ║");
+                                            System.out.println("║ 2. Boski promień - oślep wskazanego przeciwnika przez co nie będzie mógł wykonać żadnej akcji. ║");
+                                            System.out.println("║ 3. Oświecenie - oślep wszystkich przeciwników.                                                 ║");
+                                            System.out.println("╚════════════════════════════════════════════════════════════════════════════════════════════════╝");
+                                        }
+                                        case (6) -> {
+                                            System.out.println("╔════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗");
+                                            System.out.println("║                                                                     Magia Krwi                                                                     ║");
+                                            System.out.println("╠════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╣");
+                                            System.out.println("║ Zaklęcia ofensywne kosztujące życie.                                                                                                               ║");
+                                            System.out.println("║ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  ║");
+                                            System.out.println("║ 1. Krwawy akt - wskazanemu wrogowi zostaje zniszczony pancerz.                                                                                     ║");
+                                            System.out.println("║ 2. Krwawa więź - powiąż więzami krwi dwóch wskazanych wrógów. Zadanie obrażeń jednemu z tych wrogów sprawi że ten drugi również otrzyma obrażenia. ║");
+                                            System.out.println("║ 3. Krwawy rytuał - wszyscy wrogowie otrzymują średnią ilość obrażeń.                                                                               ║");
+                                            System.out.println("╚════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝");
+                                        }
+                                        case (7) -> {
+                                            System.out.println("╔═══════════════════════════════════════════════════════════════════════════════════════════════╗");
+                                            System.out.println("║                                        Magia Błyskawic                                        ║");
+                                            System.out.println("╠═══════════════════════════════════════════════════════════════════════════════════════════════╣");
+                                            System.out.println("║ Duże obrażenia!                                                                               ║");
+                                            System.out.println("║ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ║");
+                                            System.out.println("║ 1. Piorun strzelisty - jeden przeciwnik otrzymuje średnią ilość obrażeń.                      ║");
+                                            System.out.println("║ 2. Strzelista włócznia - wskazana kolumna przeciwników otrzymuje średnie obrażenia.           ║");
+                                            System.out.println("║ 3. Gniew Magnira - jeden przeciwnik otrzymuje losową ilość obrażeń.                           ║");
+                                            System.out.println("╚═══════════════════════════════════════════════════════════════════════════════════════════════╝\n");
+                                        }
+                                        default -> System.out.println("Nie ma takiej opcji");
+                                    }
 
                                 }
+
                                 default -> System.out.println("Nie ma takiej opcji");
                             }
                         }
