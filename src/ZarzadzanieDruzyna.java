4dbcb48caf4e700b277b88aa0aa602a91bd49459
Korreq <55250560+Korreq@users.noreply.github.com>
2020-11-18 13:50:00 +0100

    zarządzanko

--- src/ZarzadzanieDruzyna.java
+++ src/ZarzadzanieDruzyna.java
@@ -1,13 +1,15 @@
 
 import java.util.Scanner;
 
-public class ZarzadzanieDruzyna {
-
-    public static void zarzadzanie(){
+public class ZarzadzanieDruzyna
+{
+    public static void zarzadzanie()
+    {
         System.out.println("\nPo wyjściu z karczmy masz chwile dla siebie wybierz co chcesz zrobic\n");
         Scanner scanner = new Scanner(System.in);
         boolean oboz = false;
-        while(!oboz){
+        while(!oboz)
+        {
             System.out.println("╔═══════════════════════════════╗");
             System.out.println("║       Co chcesz zrobić:       ║");
             System.out.println("╠═══════════════════════════════╣");
@@ -17,69 +19,67 @@ public class ZarzadzanieDruzyna {
             System.out.println("╚═══════════════════════════════╝");
             char choice = scanner.next().charAt(0);
             char choice2 = Character.toLowerCase(choice);
-        switch (choice2){
-            case('a')->{
-                System.out.println("╔══════════════════╗");
-                System.out.println("║ Twoja drużyna    ║");
-                System.out.println("╠══════════════════╣");
-                for (int i = 0; i < Team.number; i++) {
-                    int p1=i+1;
-                    System.out.println("║ [" + p1 + "] • " + Team.team[i]+"   ║");
+            switch (choice2)
+            {
+                case('a') -> {
+                            System.out.println("╔══════════════════╗");
+                            System.out.println("║ Twoja drużyna    ║");
+                            System.out.println("╠══════════════════╣");
+                            for (int i = 0; i < Team.number; i++) { int p1=i+1;System.out.println("║ [" + p1 + "] • " + Team.team[i]+"   ║"); }
+                            System.out.println("╚══════════════════╝\n");
                 }
-                System.out.println("╚══════════════════╝\n");
+                case('b')->{
+                            if(Team.number == 1){ System.out.println("Masz tylko 1 członka");break; }
+                            System.out.println("╔══════════════════╗");
+                            System.out.println("║ Twoja drużyna    ║");
+                            System.out.println("╠══════════════════╣");
+                            for (int i = 0; i < Team.number; i++) { int p1=i+1;System.out.println("║ [" + p1 + "] • " + Team.team[i]+"   ║"); }
+                            System.out.println("╚══════════════════╝\n");
+                            System.out.println("Kogo chcesz wymienić:");
+                            int wyb;
+                            while (!scanner.hasNextInt())
+                            {
+                                System.out.println("╔══════════════╗");
+                                System.out.println("║ To nie numer ║");
+                                System.out.println("╚══════════════╝");
+                                scanner.next();
+                            }
+                            wyb = scanner.nextInt();
+                            if(wyb > Team.number || wyb <= 0)
+                            {
+                                System.out.println("╔═══════════════════════════════════════════╗");
+                                System.out.println("║ Nie ma takiego członka! Spróbuj ponownie. ║");
+                                System.out.println("╚═══════════════════════════════════════════╝\n");
+                                break;
+                            }
+                            System.out.println("Na kogo:");
+                            int wyb2;
+                            while (!scanner.hasNextInt())
+                            {
+                                System.out.println("╔══════════════╗");
+                                System.out.println("║ To nie numer ║");
+                                System.out.println("╚══════════════╝");
+                                scanner.next();
+                            }
+                            wyb2 = scanner.nextInt();
+                            if(wyb2 > Team.number || wyb2 <= 0)
+                            {
+                                System.out.println("╔═══════════════════════════════════════════╗");
+                                System.out.println("║ Nie ma takiego członka! Spróbuj ponownie. ║");
+                                System.out.println("╚═══════════════════════════════════════════╝\n");
+                                break;
+                            }
+                            Team.bufor[0] = Team.team[wyb-1];
+                            Team.team[wyb-1] = Team.team[wyb2-1];
+                            Team.team[wyb2-1] = Team.bufor[0];
+                }
+                case('c') -> oboz = true;
+                default -> {
+                    System.out.println("╔════════════════════════════════════════╗");
+                    System.out.println("║ Nie ma takiej opcji! Spróbuj ponownie. ║");
+                    System.out.println("╚════════════════════════════════════════╝\n");
+                }
             }
-            case('b')->{
-                if(Team.number == 1){
-                    System.out.println("Masz tylko 1 członka");
-                    break;
-                }
-                System.out.println("╔══════════════════╗");
-                System.out.println("║ Twoja drużyna    ║");
-                System.out.println("╠══════════════════╣");
-                for (int i = 0; i < Team.number; i++) {
-                    int p1=i+1;
-                    System.out.println("║ [" + p1 + "] • " + Team.team[i]+"   ║");
-                }
-                System.out.println("╚══════════════════╝\n");
-                System.out.println("Kogo chcesz wymienić:");
-                int wyb;
-                    while (!scanner.hasNextInt()) {
-                        System.out.println("╔══════════════╗");
-                        System.out.println("║ To nie numer ║");
-                        System.out.println("╚══════════════╝");
-                        scanner.next(); // this is important!
-                    }
-                    wyb = scanner.nextInt();
-                if(wyb > Team.number || wyb <= 0){
-                    System.out.println("╔═══════════════════════════════════════════╗");
-                    System.out.println("║ Nie ma takiego członka! Spróbuj ponownie. ║");
-                    System.out.println("╚═══════════════════════════════════════════╝\n");
-                    break;
-                }
-                System.out.println("Na kogo:");
-                int wyb2;
-                    while (!scanner.hasNextInt()) {
-                        System.out.println("╔══════════════╗");
-                        System.out.println("║ To nie numer ║");
-                        System.out.println("╚══════════════╝");
-                        scanner.next(); // this is important!
-                    }
-                    wyb2 = scanner.nextInt();
-                if(wyb2 > Team.number || wyb2 <= 0){
-                    System.out.println("╔═══════════════════════════════════════════╗");
-                    System.out.println("║ Nie ma takiego członka! Spróbuj ponownie. ║");
-                    System.out.println("╚═══════════════════════════════════════════╝\n");
-                    break;
-                }
-                Team.bufor[0] = Team.team[wyb-1];
-                Team.team[wyb-1] = Team.team[wyb2-1];
-                Team.team[wyb2-1] = Team.bufor[0];
-            }
-            case('c')-> oboz = true;
-            default -> {
-                System.out.println("╔════════════════════════════════════════╗");
-                System.out.println("║ Nie ma takiej opcji! Spróbuj ponownie. ║");
-                System.out.println("╚════════════════════════════════════════╝\n");}
         }
-    }}
+    }
 }
